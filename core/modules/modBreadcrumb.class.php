a99171ae0e85230d990c2732a74d53dcd5e8c35d
arnaud <arnaud.saint-patrice@atm-consulting.fr>
2015-09-23 10:41:18 +0200

    FIX : debug javascript passage 1.2

--- core/modules/modBreadcrumb.class.php
+++ core/modules/modBreadcrumb.class.php
@@ -61,7 +61,7 @@ class modBreadcrumb extends DolibarrModules
         // (where XXX is value of numeric property 'numero' of module)
         $this->description = "Module de fil d'ariane";
         // Possible values for version are: 'development', 'experimental' or version
-        $this->version = '1.1';
+        $this->version = '1.2';
         // Key used in llx_const table to save module status enabled/disabled
         // (where MYMODULE is value of property name of module in uppercase)
         $this->const_name = 'MAIN_MODULE_' . strtoupper($this->name);
